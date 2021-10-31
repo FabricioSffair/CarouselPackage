cabd9e6c96bfbd8c9183c3def6e577114bad266d
Fabricio <fabriciosffair@gmail.com>
2021-10-31 11:03:51 -0300

    Merged NextButton branch

--- Sources/UnflowInterviewApp/Carousel.swift
+++ Sources/UnflowInterviewApp/Carousel.swift
@@ -3,12 +3,13 @@ import SwiftUI
 public struct Carousel: View {
     
     public init() {}
-    
+
+    // TODO: Retrieve info from config file
     public var body: some View {
         CarouselView(pages: [
-            .standard(title: "Test", text: "Test", image: "image1", backgroundImage: "bg1"),
-            .standard(title: "Test", text: "Test", image: "image2", backgroundImage: "bg2"),
-            .standard(title: "Test", text: "Test", image: "image3", backgroundImage: "bg3")
+            .standard(title: "Test", text: "Use Audio Rooms for fun activities like socials. Turn on Boogie Mode to hve a random song play and so you can dance and get your heart pumping - the best part, nobody can see you!", image: "image1", buttonTitle: "Next", backgroundImage: "bg1"),
+            .standard(title: "Test", text: "Test", image: "image2", buttonTitle: "Next", backgroundImage: "bg2"),
+            .standard(title: "Test", text: "Test", image: "image3", buttonTitle: nil, backgroundImage: "bg3")
         ])
     }
 }
